f26393b4fe9222100a0d027e9b43dca3fcaa96a2
Alexander Walford <74402359+alexanderwalford-official@users.noreply.github.com>
2025-02-19 12:24:08 +0000

    Added some basic tests.

--- main.py
+++ main.py
@@ -8,7 +8,10 @@ from fastapi import FastAPI
 from models import *
 from db import *
 
+# create the app object
 app = FastAPI()
+
+# routes are as followed:
 
 @app.get("/")
 def read_root():
@@ -22,13 +25,21 @@ def read_user(user_id: int):
     """
         This function reads a user from the database.
     """
-    obj = User(id=user_id, name="John Doe", age=25)
-    return {"user_id": user_id}
+    obj = User(id=user_id, name="John Doe", age=25) # temp
+    # some super quick and simple validation
+    assert obj.id > 0, "ID must be greater than 0."
+    assert obj.age > 0, "Age must be greater than 0."
+    assert obj.name != "", "Name must not be empty."
+    return {"user_id": obj.id, "name": obj.name, "age": obj.age}
 
 @app.post("/users/")
-def create_user(user: User):
+def create_user(id: int, name: str, age: int):
     """
         This function creates a user in the database.
     """
+    assert id > 0, "ID must be greater than 0."
+    assert name != "", "Name must not be empty."
+    assert age > 0, "Age must be greater than 0."
+    user = User(id=id, name=name, age=age)
     create_user("user", user)
     return user
